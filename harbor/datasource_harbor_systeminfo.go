e78f7c34fde752542166908059947d4e6f46d3df
Matt Burdan <burdz@burdz.net>
2020-03-12 13:00:31 -0700

    add harbor_system_configuration_auth resource (#7)

--- harbor/datasource_harbor_systeminfo.go
+++ harbor/datasource_harbor_systeminfo.go
@@ -5,6 +5,7 @@ import (
 
 	apiclient "github.com/sandhose/terraform-provider-harbor/api/client"
 	"github.com/sandhose/terraform-provider-harbor/api/client/products"
+	"github.com/sandhose/terraform-provider-harbor/api/models"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -53,9 +54,30 @@ func dataSourceHarborSystemInfo() *schema.Resource {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
+			"next_scan_all": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
 			"harbor_version": {
 				Type:     schema.TypeString,
 				Computed: true,
+			},
+			"clair_vulnerability_status": {
+				Type:     schema.TypeList,
+				Computed: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"overall_last_update": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						// "details": {
+						// 	Type:     schema.TypeInt,
+						// 	Computed: true,
+						// },
+					},
+				},
 			},
 		},
 	}
@@ -64,24 +86,47 @@ func dataSourceHarborSystemInfo() *schema.Resource {
 func dataSourceHarborSystemInfoRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*apiclient.Harbor)
 	resp, err := client.Products.GetSysteminfo(products.NewGetSysteminfoParamsWithContext(context.TODO()), nil)
-
 	if err != nil {
 		return err
 	}
 
 	i := resp.Payload
-	// TODO: unique ID?
+
+	// TODO(burdz): unique ID <10-03-20> //
 	d.SetId("systeminfo")
 	d.Set("admiral_endpoint", i.AdmiralEndpoint)
 	d.Set("auth_mode", i.AuthMode)
 	d.Set("external_url", i.ExternalURL)
 	d.Set("harbor_version", i.HarborVersion)
 	d.Set("has_ca_root", i.HasCaRoot)
+	d.Set("next_scan_all", i.NextScanAll)
 	d.Set("project_creation_restriction", i.ProjectCreationRestriction)
 	d.Set("registry_url", i.RegistryURL)
 	d.Set("self_registration", i.SelfRegistration)
 	d.Set("with_admiral", i.WithAdmiral)
 	d.Set("with_clair", i.WithClair)
 	d.Set("with_notary", i.WithNotary)
+
+	clairVulnerabilityStatus, err := flattenClairVulnerabilityStatus(i.ClairVulnerabilityStatus, d)
+	if err != nil {
+		return err
+	}
+
+	if clairVulnerabilityStatus != nil {
+		if err := d.Set("clair_vulnerability_status", clairVulnerabilityStatus); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func flattenClairVulnerabilityStatus(i *models.GeneralInfoClairVulnerabilityStatus, d *schema.ResourceData) (map[string]interface{}, error) {
+	if i == nil {
+		return nil, nil
+	}
+
+	clairStatus := make(map[string]interface{})
+
+	clairStatus["overall_last_update"] = i.OverallLastUpdate
+	return clairStatus, nil
 }
--- harbor/provider.go
+++ harbor/provider.go
@@ -6,46 +6,69 @@ import (
 )
 
 func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
+	provider := &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"host": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("HARBOR_HOST", nil),
-				Description: "The hostname (in form of URI) of the Harbor server.",
+				Description: descriptions["host"],
 			},
 			"username": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("HARBOR_USERNAME", nil),
-				Description: "The username to use for HTTP basic authentication when accessing the Harbor server.",
+				Description: descriptions["username"],
 			},
 			"password": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("HARBOR_PASSWORD", nil),
-				Description: "The password to use for HTTP basic authentication when accessing the Harbor server.",
+				Description: descriptions["password"],
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"harbor_project":               resourceHarborProject(),
-			"harbor_project_robot_account": resourceHarborProjectRobotAccount(),
-			"harbor_usergroup":             resourceHarborUserGroup(),
+			"harbor_project":                   resourceHarborProject(),
+			"harbor_project_robot_account":     resourceHarborProjectRobotAccount(),
+			"harbor_usergroup":                 resourceHarborUserGroup(),
+			"harbor_system_configuration_auth": resourceHarborConfigAuth(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"harbor_system_info": dataSourceHarborSystemInfo(),
 			"harbor_project":     dataSourceHarborProject(),
 			"harbor_usergroup":   dataSourceHarborUserGroup(),
 		},
-		ConfigureFunc: providerConfigure,
 	}
+
+	provider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
+		terraformVersion := provider.TerraformVersion
+		if terraformVersion == "" {
+			// Terraform 0.12 introduced this field to the protocol
+			// We can therefore assume that if it's missing it's 0.10 or 0.11
+			terraformVersion = "0.11+compatible"
+		}
+		return providerConfigure(d, terraformVersion)
+	}
+
+	return provider
 }
 
-func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+func providerConfigure(d *schema.ResourceData, terraformVersion string) (interface{}, error) {
 	cfg := &Config{
-		Host:     d.Get("host").(string),
-		Username: d.Get("username").(string),
-		Password: d.Get("password").(string),
+		Host:             d.Get("host").(string),
+		Username:         d.Get("username").(string),
+		Password:         d.Get("password").(string),
+		terraformVersion: terraformVersion,
 	}
 	return cfg.Config()
+}
+
+var descriptions map[string]string
+
+func init() {
+	descriptions = map[string]string{
+		"host":     "The hostname (in form of URI) of the Harbor server.",
+		"username": "The username to use for HTTP basic authentication when accessing the Harbor server.",
+		"password": "The password to use for HTTP basic authentication when accessing the Harbor server.",
+	}
 }
